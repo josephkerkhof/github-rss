1b7f40880bbb82733a59011407c0106403a2884b
Joseph Kerkhof <joseph@kerkhof.dev>
2025-08-10 13:25:50 -0500

    add fillable attributes

--- app/Console/Commands/FetchPullRequests.php
+++ app/Console/Commands/FetchPullRequests.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\GitHubUser;
-use App\Models\Label;
+use App\Models\Author;
+use App\Models\Branch;
 use App\Models\PullRequest;
 use App\Models\Repository;
 use GrahamCampbell\GitHub\Facades\GitHub;
@@ -31,19 +31,73 @@ class FetchPullRequests extends Command
      */
     public function handle(): bool
     {
-        $mergedPullRequests = GitHub::search()->issues(
-            'repo:laravel/framework is:pr is:merged',
-        );
-
-        $mergedPullRequestsUrls = collect($mergedPullRequests['items'])->pluck('pull_request.html_url');
-
-        foreach ($mergedPullRequestsUrls as $url) {
-            $id = Str::of($url)->afterLast('/')->toString();
-
-            $pullRequestDetails = GitHub::pullRequests()->show('laravel', 'framework', $id);
-            dd($pullRequestDetails);
-        }
+        Repository::all()->chunk(100)->each(function ($chunk) {
+            foreach ($chunk as $repository) {
+                $this->fetchByRepository($repository);
+            }
+        });
 
         return Command::SUCCESS;
+    }
+
+    private function fetchByRepository(Repository $repository): void
+    {
+        $this->info("Fetching merged pull requests for repository: {$repository->name}");
+
+        $pullRequests = GitHub::search()->issues(
+            "repo:{$repository->slug} is:pr is:merged",
+        );
+        $mergedPullRequestsUrls = collect($pullRequests['items'])->pluck('pull_request.html_url');
+
+        foreach ($mergedPullRequestsUrls as $url) {
+            $this->fetchMergeRequestByUrl($repository, $url);
+        }
+    }
+
+    private function fetchMergeRequestByUrl(Repository $repository, string $url): void
+    {
+        $number = Str::of($url)->afterLast('/')->toString();
+
+        $pullRequestAlreadyExists = PullRequest::query()
+            ->whereBelongsTo($repository)
+            ->where('number', $number)
+            ->exists();
+
+        if ($pullRequestAlreadyExists) {
+            $this->info("Pull request #{$number} already exists. Skipping.");
+
+            return;
+        }
+
+        $pullRequestDetails = GitHub::pullRequests()->show('laravel', 'framework', $number);
+
+        $branch = Branch::firstOrCreate(
+            [
+                'repository_id' => $repository->id,
+                'name' => $pullRequestDetails['base']['ref'],
+            ]
+        );
+
+        $author = Author::firstOrCreate(
+            [
+                'username' => $pullRequestDetails['user']['login']
+            ],
+            [
+                'profile_url' => $pullRequestDetails['user']['html_url'],
+            ]
+        );
+
+        PullRequest::create([
+            'repository_id' => $repository->id,
+            'branch_id' => $branch->id,
+            'author_id' => $author->id,
+            'number' => $number,
+            'title' => $pullRequestDetails['title'],
+            'body' => $pullRequestDetails['body'],
+            'url' => $pullRequestDetails['html_url'],
+            'merged_at' => $pullRequestDetails['merged_at'],
+        ]);
+
+        $this->info("Pull request #{$number} fetched successfully.");
     }
 }
--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -11,6 +11,7 @@ class Branch extends Model
     use HasFactory;
 
     protected $fillable = [
+        'repository_id',
         'name',
     ];
 }
