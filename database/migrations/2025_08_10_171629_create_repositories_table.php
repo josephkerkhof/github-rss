5bab3b235f466b1f65489319cc92474f2741a0c1
Joseph Kerkhof <joseph@kerkhof.dev>
2025-08-10 12:31:57 -0500

    add repository

--- database/migrations/2025_08_10_171629_create_repositories_table.php
+++ database/migrations/2025_08_10_171629_create_repositories_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
+
+            $table->unique('slug');
         });
     }
 
