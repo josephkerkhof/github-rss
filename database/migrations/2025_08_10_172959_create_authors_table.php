8969e307ca0af96d489693402409f6ef22b74fb7
Joseph Kerkhof <joseph@kerkhof.dev>
2025-08-10 12:34:12 -0500

    add author

---
+++ database/migrations/2025_08_10_172959_create_authors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('profile_url');
+            $table->timestamps();
+
+            $table->unique('username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
