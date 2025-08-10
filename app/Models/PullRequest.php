14e435a770c07d2cf6f078e4a9aec81f0c7a4c10
Joseph Kerkhof <joseph@kerkhof.dev>
2025-08-10 13:11:51 -0500

    add PullRequest model definitions

--- app/Models/PullRequest.php
+++ app/Models/PullRequest.php
@@ -4,9 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PullRequest extends Model
 {
     /** @use HasFactory<\Database\Factories\PullRequestFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'title',
+        'body',
+        'url',
+        'merged_at',
+    ];
+
+    protected $casts = [
+        'merged_at' => 'datetime',
+    ];
+
+    public function repository(): BelongsTo
+    {
+        return $this->belongsTo(Repository::class);
+    }
+
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
