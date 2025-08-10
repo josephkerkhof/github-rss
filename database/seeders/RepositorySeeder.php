ace10a6cac0376df70637b336dd62b060836cb32
Joseph Kerkhof <joseph@kerkhof.dev>
2025-08-10 12:26:18 -0500

    add repository

---
+++ database/seeders/RepositorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Repository;
+use Illuminate\Database\Seeder;
+
+class RepositorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Repository::factory()->create();
+    }
+}
