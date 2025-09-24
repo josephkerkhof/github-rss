0a2ca73a100ad18ca3da7242f2cf8a0d51f08c85
Joseph Kerkhof <joseph@kerkhof.dev>
2025-09-24 14:28:49 -0500

    add user to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,17 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Joseph Kerkhof',
+            'email' => 'joseph@kerkhof.dev',
+            'password' => Hash::make('Testing123'),
         ]);
     }
 }
