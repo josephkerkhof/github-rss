f6841d5a801086fcc83fff6fc1d8a3df529e14f7
Joseph Kerkhof <joseph@kerkhof.dev>
2025-09-24 13:44:26 -0500

    add slug property to the repository model

--- app/Models/Repository.php
+++ app/Models/Repository.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Iksaku\Laravel\MassUpdate\MassUpdatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $name
+ * @property string $owner
+ * @property string $repo
+ */
 class Repository extends Model
 {
     /** @use HasFactory<\Database\Factories\RepositoryFactory> */
@@ -17,4 +23,11 @@ class Repository extends Model
         'owner',
         'repo'
     ];
+
+    protected function slug(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->owner . '/' . $this->repo,
+        );
+    }
 }
