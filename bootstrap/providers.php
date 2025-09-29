3dea5c45da877adf49d4ab520ac3934ea2fd29ce
Joseph Kerkhof <joseph@kerkhof.dev>
2025-09-29 18:18:58 -0500

    remove filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,7 @@
 declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
-use App\Providers\Filament\AdminPanelProvider;
 
 return [
     AppServiceProvider::class,
-    AdminPanelProvider::class,
 ];
