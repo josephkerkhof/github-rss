38e96c589a702ca381894eba74aff0fc362138eb
Joseph Kerkhof <joseph@kerkhof.dev>
2025-10-06 20:12:12 -0500

    add Laravel Sanctum for API authentication and implement related routes, controllers, and configurations

--- resources/js/actions/App/Http/Controllers/Settings/index.ts
+++ resources/js/actions/App/Http/Controllers/Settings/index.ts
@@ -1,11 +1,13 @@
 import ProfileController from './ProfileController'
 import PasswordController from './PasswordController'
 import TwoFactorAuthenticationController from './TwoFactorAuthenticationController'
+import TokenController from './TokenController'
 
 const Settings = {
     ProfileController: Object.assign(ProfileController, ProfileController),
     PasswordController: Object.assign(PasswordController, PasswordController),
     TwoFactorAuthenticationController: Object.assign(TwoFactorAuthenticationController, TwoFactorAuthenticationController),
+    TokenController: Object.assign(TokenController, TokenController),
 }
 
 export default Settings
