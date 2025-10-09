57df09cb5f5e15f6259aabf875a9b1f31e7debac
Joseph Kerkhof <joseph@kerkhof.dev>
2025-10-09 10:00:46 -0500

    allow the body of the pull request data to be nullable

--- app/Domains/PullRequests/Schema/PullRequestData.php
+++ app/Domains/PullRequests/Schema/PullRequestData.php
@@ -17,7 +17,7 @@ final class PullRequestData extends Data
         public readonly string $targetBranchName,
         public readonly int $number,
         public readonly string $title,
-        public readonly string $body,
+        public readonly ?string $body,
         public readonly string $url,
         public readonly CarbonImmutable $mergedAt,
     ) {
