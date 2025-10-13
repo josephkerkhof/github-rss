b0308a4e13735f24f7ccd2e41950a7334929c3df
Joseph Kerkhof <joseph@kerkhof.dev>
2025-10-13 13:17:15 -0500

    fix console configuration to run the fetch pull request every 15 minutes

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchPullRequests;
 
-Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(FetchPullRequests::class)
+    ->withoutOverlapping()
+    ->everyFifteenMinutes();
